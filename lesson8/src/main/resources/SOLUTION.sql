de40ebed959e4d33dcee407b6ece451ca66fef9e
alex96jvm <alex96jvm@gmail.com>
2024-09-10 14:32:01 +0300

    Complete Lesson 8 tasks

---
+++ lesson8/src/main/resources/SOLUTION.sql
@@ -0,0 +1,7 @@
+SELECT MIN(birthday) FROM student;
+
+SELECT MIN(payment_date) FROM payment;
+
+SELECT AVG(mark) FROM mark JOIN subject ON mark.subject_id = subject.id WHERE subject.name = 'Math';
+
+SELECT MIN(amount) FROM payment JOIN paymenttype ON payment.type_id = paymenttype.id WHERE paymenttype.name = 'WEEKLY';
