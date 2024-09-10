8291c81294918460a641f070ba7146692a390f40
alex96jvm <alex96jvm@gmail.com>
2024-09-10 11:17:13 +0300

    Complete Lesson 3 tasks

---
+++ lesson3/src/main/resources/SOLUTION.sql
@@ -0,0 +1,15 @@
+ALTER TABLE student ALTER COLUMN birthday DATE NOT NULL;
+
+ALTER TABLE mark ADD CONSTRAINT mark CHECK (mark > 0 AND mark <=10);
+
+ALTER TABLE mark ALTER COLUMN student_id BIGINT NOT NULL;
+
+ALTER TABLE mark ALTER COLUMN subject_id BIGINT NOT NULL;
+
+ALTER TABLE subject ADD CONSTRAINT grade CHECK (grade > 0 AND grade <= 5);
+
+ALTER TABLE paymenttype ADD CONSTRAINT unique_name UNIQUE (name);
+
+ALTER TABLE payment ALTER COLUMN amount DECIMAL NOT NULL;
+
+ALTER TABLE payment ALTER COLUMN payment_date DATETIME NOT NULL;
