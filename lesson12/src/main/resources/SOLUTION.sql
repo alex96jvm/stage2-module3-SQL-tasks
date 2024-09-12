4926f0dc520874e1ee4cb1dba67e0f6562521b4a
alex96jvm <alex96jvm@gmail.com>
2024-09-12 23:41:05 +0300

    Fix lesson 10 tasks

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
-DELETE FROM student WHERE id IN (SELECT student_id FROM mark GROUP BY student_id HAVING AVG(mark) >= 4);
+DELETE FROM student WHERE id IN (SELECT DISTINCT mark.student_id FROM student JOIN mark ON student.id = mark.student_id WHERE mark >= 4);
 
 DELETE FROM student WHERE id IN (SELECT student_id FROM mark WHERE mark < 4);
 
