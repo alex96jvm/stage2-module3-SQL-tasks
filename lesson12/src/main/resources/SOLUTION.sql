57b5274739c45b8116e10309ecf13c52d9fa70b3
alex96jvm <alex96jvm@gmail.com>
2024-09-13 00:35:19 +0300

    Fix first query

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
-DELETE FROM student WHERE id IN (SELECT DISTINCT mark.student_id FROM student JOIN mark ON student.id = mark.student_id WHERE mark >= 4);
+DELETE FROM student WHERE id IN (SELECT DISTINCT m.student_id FROM mark AS m JOIN subject AS s on s.id = m.subject_id WHERE grade >= 4);
 
 DELETE FROM student WHERE id IN (SELECT student_id FROM mark WHERE mark < 4);
 
--- lesson12/src/main/resources/TABLES_CHANGES.sql
+++ lesson12/src/main/resources/TABLES_CHANGES.sql
@@ -1,12 +1,12 @@
 ALTER TABLE mark DROP CONSTRAINT mark_student_id_fkey;
-
 ALTER TABLE mark ADD CONSTRAINT mark_student_id_fkey FOREIGN KEY (student_id) REFERENCES student(id) ON DELETE CASCADE;
 
-ALTER TABLE payment DROP CONSTRAINT payment_student_id_fkey;
+ALTER TABLE Mark DROP CONSTRAINT IF EXISTS mark_subject_id_fkey;
+ALTER TABLE Mark ADD CONSTRAINT mark_subject_id_fkey FOREIGN KEY (subject_id) REFERENCES Subject(id) ON DELETE CASCADE;
 
+ALTER TABLE payment DROP CONSTRAINT payment_student_id_fkey;
 ALTER TABLE payment ADD CONSTRAINT payment_student_id_fkey FOREIGN KEY (student_id) REFERENCES student(id) ON DELETE CASCADE;
 
 ALTER TABLE payment DROP CONSTRAINT payment_type_id_fkey;
-
 ALTER TABLE payment ADD CONSTRAINT payment_type_id_fkey FOREIGN KEY (type_id) REFERENCES paymenttype(id) ON DELETE CASCADE;
 
