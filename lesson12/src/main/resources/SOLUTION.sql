088841be787251dec1d78d316039c66f7ec4865e
alex96jvm <alex96jvm@gmail.com>
2024-09-12 23:46:15 +0300

    Fix lesson 12 tasks

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -4,5 +4,5 @@ DELETE FROM student WHERE id IN (SELECT student_id FROM mark WHERE mark < 4);
 
 DELETE FROM paymenttype WHERE name = 'DAILY';
 
-DELETE FROM mark WHERE mark < 7;
+DELETE FROM mark WHERE mark IN (0, 6);
 
